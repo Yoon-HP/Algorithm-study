88f6acb54c9cbd0b9a7ab31149e7315da663a9d4
Yoon Hwa-pyeong <88183316+Yoon-HP@users.noreply.github.com>
2022-04-19 01:23:09 +0900

    [Bronze I] Title: 2차원 배열의 합, Time: 408 ms, Memory: 1344 KB -BaekjoonHub

--- 백준/Bronze/2167. 2차원 배열의 합/2차원 배열의 합.c
+++ 백준/Bronze/2167. 2차원 배열의 합/2차원 배열의 합.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int N, M;
     scanf("%d %d", &N, &M);
-    int X[N][M],i,j;
-    for (i = 0; i < N;i++){
-        for (j = 0; j < M;j++){
+    int X[N][M], i, j;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
             scanf("%d", &X[i][j]);
         }
     }
-    int k,x,y;
+    int k, x, y;
     scanf("%d", &k);
-    for (int h = 0; h < k;h++){
+    for (int h = 0; h < k; h++)
+    {
         scanf("%d %d %d %d", &i, &j, &x, &y);
         i--;
         j--;
         x--;
         y--;
         int sum = 0;
-        for (int q = 0; q < N;q++){
-            for (int w = 0; w < M;w++){
-                if (q>=i && q<=x && w>=j && w<=y){
-                    sum += X[q][w];
-                }
+        for (int q = i; q <= x; q++)
+        {
+            for (int w = j; w <= y; w++)
+            {
+                sum += X[q][w];
             }
         }
         printf("%d\n", sum);
     }
+
     return 0;
 }
