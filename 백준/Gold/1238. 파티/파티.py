1a26681bf02c15830cefe1d08f71b8a7ad75ab64
Yoon Hwa-pyeong <88183316+Yoon-HP@users.noreply.github.com>
2022-07-01 02:20:18 +0900

    [Gold III] Title: 파티, Time: 88 ms, Memory: 33932 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -3,33 +3,33 @@ import sys
 input=sys.stdin.readline
 
 N,M,X=map(int,input().split())
-path=[[] for _ in range(N+1)]
+front=[[] for _ in range(N+1)]
+back=[[] for _ in range(N+1)]
 for _ in range(M):
     u,v,w=map(int,input().split())
-    path[u].append([v,w])
+    # 노드 X에서 각 노드로 가는 최단경로를 구하는데 사용
+    front[u].append([v,w])
+    # 각 노드에서 노드 X까지 가는 최단경로를 구하는데 사용
+    back[v].append([u,w])
 
-
-def dijkstra(start):
-    min_Q=[]
-    stpath[start]=0
-    heapq.heappush(min_Q,[0,start])
+def dijkstra(graph):
+    min_Q=[[0,X]]
+    stpath=[float('inf')]*(N+1)
+    stpath[X]=0
     while min_Q:
         d,u=heapq.heappop(min_Q)
-        if start!=X and u==X:
-            break
-        for v,w in path[u]:
+        # 이미 최단경로가 확정된 노드에 대해 불필요한 연산을 줄임
+        if stpath[u]<d:
+            continue
+        for v,w in graph[u]:
             temp_w=w+d
             if temp_w<stpath[v]:
                 stpath[v]=temp_w
-                heapq.heappush(min_Q,[temp_w,v])    
+                heapq.heappush(min_Q,[temp_w,v])
+    for i in range(1,len(stpath)):
+        result[i]+=stpath[i]
+                
 result=[0]*(N+1)
-stpath=[float('inf')]*(N+1)
-dijkstra(X)
-for i in range(len(stpath)):
-    result[i]+=stpath[i]
-for i in range(1,N+1):
-    stpath=[float('inf')]*(N+1)
-    dijkstra(i)
-    result[i]+=stpath[X]
-result[0]=0
+dijkstra(front)
+dijkstra(back)
 print(max(result))
