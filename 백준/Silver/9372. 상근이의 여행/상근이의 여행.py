0147132e3312aea2c8f6c9e3960b4824c6b0a168
Yoon Hwa-pyeong <88183316+Yoon-HP@users.noreply.github.com>
2022-08-04 03:01:05 +0900

    [Silver IV] Title: 상근이의 여행, Time: 188 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,9 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+for _ in range(t):
+    N,M=map(int,input().split())
+    for i in range(M):
+        u,v=map(int,input().split())
+    print(N-1)
