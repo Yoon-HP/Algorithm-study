0a98d252ec76f217e1dd2927e7bd58190b49fa93
Yoon Hwa-pyeong <88183316+Yoon-HP@users.noreply.github.com>
2023-04-04 17:23:50 +0900

    [Gold IV] Title: 불, Time: 4656 ms, Memory: 115604 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.py
@@ -0,0 +1,76 @@
+# 5427번 불
+from collections import deque
+import sys
+input=sys.stdin.readline
+# 아이디어 불이 확산된 모양을 먼저 계산한 후 상근이를 이동하면 될듯
+dx=[1,-1,0,0]
+dy=[0,0,1,-1]
+T=int(input())
+for _ in range(T):
+    w,h=map(int,input().split())
+    w,h=h,w
+    Map=[]
+    me=[]
+    fire=[]
+    for row in range(w):
+        temp=list(input().strip())
+        for index in range(len(temp)):
+            # 지훈이 초기 위치
+            if temp[index]=='@':
+                me.append([row,index])
+            elif temp[index]=='*':
+                fire.append([row,index])
+        Map.append(temp)
+
+    # 불이 퍼져나가는 양상 Map에 추가
+    f_visit=[[-1]*h for _ in range(w)]
+
+    q=deque([])
+    for f in fire:
+        # 초기 불 위치 큐에 추가
+        q.append(f)
+        f_visit[f[0]][f[1]]=0
+        
+    while q:
+        cur=q.popleft()
+        for i in range(4):
+            nx=cur[0]+dx[i]
+            ny=cur[1]+dy[i]
+            if nx<0 or nx>=w or ny<0 or ny>=h:
+                continue
+            # 이미 방문했거나 벽인 경우엔 지나가지 못함
+            if f_visit[nx][ny]>=0 or Map[nx][ny]=='#':
+                continue
+            f_visit[nx][ny]=f_visit[cur[0]][cur[1]]+1
+            q.append([nx,ny])
+
+    q=deque([])
+    visit=[[-1]*h for _ in range(w)]
+    for m in me:
+        q.append(m)
+        visit[m[0]][m[1]]=0
+
+    ans=False
+    a_time=0
+    flag=True
+    while q and flag:
+        cur=q.popleft()
+        for i in range(4):
+            nx=cur[0]+dx[i]
+            ny=cur[1]+dy[i]
+            # 탈출에 성공한 것
+            if nx<0 or nx>=w or ny<0 or ny>=h:
+                ans=True
+                a_time=visit[cur[0]][cur[1]]+1
+                flag=False
+                break
+            if visit[nx][ny]>=0 or Map[nx][ny]=='#':
+                continue
+            if (f_visit[nx][ny]!=-1 and (f_visit[nx][ny]<=(visit[cur[0]][cur[1]]+1))):
+                continue
+            visit[nx][ny]=visit[cur[0]][cur[1]]+1
+            q.append([nx,ny])
+    if ans:
+        print(a_time)
+    else:
+        print("IMPOSSIBLE")
