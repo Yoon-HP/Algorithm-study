c68aeaeb0345ad0d797aa6b20ec313962a10be30
Yoon Hwa-pyeong <88183316+Yoon-HP@users.noreply.github.com>
2023-12-18 19:54:25 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 3004 ms, Memory: 48508 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,36 @@
+# 숨바꼭질 4
+
+from collections import deque
+
+N, K=map(int,input().split())
+
+
+# 처음위치, 초, Path
+queue=deque([[N,0,f"{N}"]])
+
+visit=[False]*200001
+
+
+while queue:
+    pos,sec,path=queue.popleft()
+    
+    if pos==K:
+        print(len(path.split())-1)
+        print(path)
+        break
+
+    for i in range(3):
+        if i==0:
+            n_pos=pos-1
+        elif i==1:
+            n_pos=pos+1
+        elif i==2:
+            n_pos=pos*2
+    
+        if n_pos<0 or n_pos>200000:
+            continue
+        if visit[n_pos]:
+            continue
+        
+        queue.append([n_pos,sec+1,path+f" {n_pos}"])
+        visit[n_pos]=True
